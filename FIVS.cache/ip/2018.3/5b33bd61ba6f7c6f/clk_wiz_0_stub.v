c7bb66132be0de7c2ccd84b71e1f4fadfc358cfe
DvJiang <98085564+DvJiang@users.noreply.github.com>
2022-07-01 21:04:32 +0800

    Add files via upload

---
+++ FIVS.cache/ip/2018.3/5b33bd61ba6f7c6f/clk_wiz_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+// Date        : Wed Jun  8 12:45:03 2022
+// Host        : LAPTOP-8OSVR4MC running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clk_60mhz, locked, clk_100mhz)
+/* synthesis syn_black_box black_box_pad_pin="clk_60mhz,locked,clk_100mhz" */;
+  output clk_60mhz;
+  output locked;
+  input clk_100mhz;
+endmodule
